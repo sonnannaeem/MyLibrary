c6a8bcaa5b4848a7aef28fc3675f5ac1dcd4512e
Sonnan Naeem <sonnan.naeem@onymos.com>
2022-06-21 15:12:45 -0700

    Replaced the binary with the source code

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MyLibrary",
     platforms: [
-            .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+            .macOS(.v10_14), .iOS(.v15), .tvOS(.v13)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -22,17 +22,26 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
-            name: "TestSDKFrameworkWrapper",
-            dependencies: [
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
-                .target(name: "TestCoreFramework"),
-            ],
-            path: "Sources/TestSDKFrameworkWrapper",
-            publicHeadersPath: ""
-        ),
-        .binaryTarget(
             name: "TestCoreFramework",
-            path: "./Sources/TestCoreFramework.xcframework")
+            dependencies: [
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
+            ],
+            path: "Sources/TestCoreFramework"
+        )
+        
+//        .target(
+//            name: "TestSDKFrameworkWrapper",
+//            dependencies: [
+//                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+//                .target(name: "TestCoreFramework"),
+//            ],
+//            path: "Sources/TestSDKFrameworkWrapper",
+//            publicHeadersPath: ""
+//        ),
+//        .binaryTarget(
+//            name: "TestCoreFramework",
+//            path: "./Sources/TestCoreFramework.xcframework")
     ]
 )
